8fb976aa28980b13b5bed8d1f126b5c6aa8330bb
DanilBond <sosgames75743@gmail.com>
2021-10-18 03:17:13 +0600

    burger menu added

--- components/burger/Burger.jsx
+++ components/burger/Burger.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from "../../images/LOGO.png";
+
+import telegram from "../../images/Vector.png";
+import whatsApp from "../../images/wh.png";
+import faceBook from "../../images/fb.png";
+ 
 
 const Burger = () => {
+    const [isActive, setActive] = useState(false);
+  
+    const toggleClass = () => {
+      setActive(!isActive);
+    };
+    
     return (
         <div className="burger">
-            <div className="icon">
+            <div className={isActive ? 'iconclose': 'icon'} onClick={toggleClass}>
                 <div></div>
                 <div></div>
                 <div></div>
+            </div>
+            <div className={isActive ? 'background': 'backgroundClosed'} onClick={toggleClass}></div>
+
+            <div className={isActive ? 'contentOpen': 'content'}>
+                <img src={logo.src} alt="" />
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Values</a>
+                <a href="#">Project start</a>
+                <a href="#">Services</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Contact</a>
+
+                <div className="links">
+                    <a href="">
+                        <img className="sideBare_contacts" src={telegram.src} alt="telegram" />
+                    </a>
+
+                    <a href="">
+                        <img className="sideBare_contacts" src={whatsApp.src} alt="whatsApp" />
+                    </a>
+
+                    <a href="">
+                        <img className="sideBare_contacts" src={faceBook.src} alt="FaceBook" />
+                    </a>
             </div>
             
-            <div className="content">
-                <span>Home</span>
-                <span>About</span>
-                <span>Values</span>
-                <span>Project start</span>
-                <span>Services</span>
-                <span>Portfolio</span>
-                <span>Contact</span>
             </div>
+            
         </div>
     );
 };
