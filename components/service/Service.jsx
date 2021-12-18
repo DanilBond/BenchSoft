c78ab9e2725f31337d503d2e92584c0ae92c4469
DanilBond <sosgames75743@gmail.com>
2021-12-18 20:05:57 +0600

    png added and border none added

--- components/service/Service.jsx
+++ components/service/Service.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import Service_block_left from "./Service_block_left/Service_block_left";
 import Service_block_right from "./Service_block_right/Service_block_right";
 import photo from "../../images/LOGO (1).png";
+import p1 from "../../images/reshotka.png";
+import p2 from "../../images/_1.png";
+import p3 from "../../images/+.png";
+import p4 from "../../images/^.png";
+import p5 from "../../images/_.png";
+import p6 from "../../images/~.png";
 
 const Service = () => {
     return (
@@ -19,31 +25,78 @@ const Service = () => {
                 <Service_block_left
                     title="Code review"
                     description="A mentor will tell you how to do magic correctly"
+                    img={p4}
+                    stylesForImg={
+                        {
+                            width: "125px",
+                            marginTop: "-50px",
+                            marginLeft: "200px"
+                        }
+                    }
                 />
                 <Service_block_right
                     title="Full development proccess"
                     description="QA, DevOps, Backend and Frontend, Project manager and Tech lead"
+                    img={p6}
+                    stylesForImg={
+                        {
+                            width: "125px",
+                            marginTop: "-10px",
+                            marginLeft: "200px"
+                        }
+                    }
                 />
 
                 <Service_block_left
                     title="Monthly interview"
                     description="Improve your skills on interviews"
+                    img={p3}
+                    stylesForImg={
+                        {
+                            width: "125px",
+                            marginTop: "-45px",
+                            marginLeft: "195px"
+                        }
+                    }
                 />
                 <Service_block_right
                     title="UI & UX design"
                     description="Creative design for any product"
+                    img={p5}
+                    stylesForImg={
+                        {
+                            width: "110px",
+                            marginTop: "-90px",
+                            marginLeft: "205px"
+                        }
+                    }
                 />
 
                 <Service_block_left
                     title="Participate in startups"
                     description="Be a part of International startups"
+                    img={p1}
+                    stylesForImg={
+                        {
+                            width: "200px",
+                            marginTop: "-45px",
+                            marginLeft: "180px"
+                        }
+                    }
                 />
                 <Service_block_right
                     title="Grow with professionals"
                     description="Grow with us"
+                    img={p2}
+                    stylesForImg={
+                        {
+                            width: "100px",
+                            marginTop: "-45px",
+                            marginLeft: "200px"
+                        }
+                    }
                 />
             </div>
-            <img src={photo.src} className="service__bg" width="750" />
         </div>
     );
 };
--- components/service/Service_block_left/Service_block_left.jsx
+++ components/service/Service_block_left/Service_block_left.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Service_block_left = ({ title, description }) => {
+const Service_block_left = ({ title, description, img, stylesForImg }) => {
     return (
-        <div className="block_left" data-aos="fade-up">
+        <div className="block_left serviceBlockData" data-aos="fade-up">
             <h2>{title}</h2>
+            <img src={img == undefined ? null : img.src} alt="" style = {stylesForImg}/>
             <p>{description}</p>
+            
         </div>
     );
 };
